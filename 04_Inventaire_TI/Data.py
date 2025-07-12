93fad867eae3a22e350041782229d3d608d10568
Pierre Olivier Paquet <pierreolivierpaquet93@gmail.com>
2025-07-12 15:56:26 -0400

    {04} +track variable to Product and derived Product classes. Ex: When instances are created with track=False, the IDs are not updated.

--- 04_Inventaire_TI/Data.py
+++ 04_Inventaire_TI/Data.py
@@ -47,12 +47,16 @@ class Product:
 	def __init__(	self, \
 			  		name: str, \
 					value: int, \
-					user: User = None ):
-		Product.__id += 1
-		self._name:str = name
+					user: User, \
+					track: bool):
+		if track == True:
+			Product.__id += 1
+			self._productId = Product.__id
+		else:
+			self._productId = -1
+		self._name: str = name
 		self._value: int = value
 		self._owner: User = user
-		self._productId = Product.__id
 
 	def getName( self ):
 		return ( self._name )
@@ -87,20 +91,24 @@ class Computer( Product ):
 	def __init__(	self, \
 			  	product_name: str, \
 				product_value: int, \
-				#product_owner: User, \
 					year: int, \
 					cpu_type: str, \
 					gpu_type: str, \
 					ram_gb: int, \
-					hd_gb: int ):
-		Computer.__id += 1
-		super().__init__( product_name, product_value )
+					hd_gb: int,
+				product_owner: User = None, \
+					track: bool = True):
+		if track == True:
+			Computer.__id += 1
+			self._id = Computer.__id
+		else:
+			self._id = -1
+		super().__init__( product_name, product_value, product_owner, track )
 		self._year: int = year
 		self._cpu_type: str = cpu_type
 		self._gpu_type: str = gpu_type
 		self._ram_gb: int = ram_gb
 		self._hd_gb: int = hd_gb
-		self._id = Computer.__id
 
 class Screen( Product ):
 	"""
@@ -126,14 +134,18 @@ class Screen( Product ):
 	def __init__(	self, \
 				product_name: str, \
 				product_value: int, \
-				#product_owner: User, \
 			  		display_size: int, \
-					hdmi_port: bool ):
-		Screen.__id += 1
-		super().__init__( product_name, product_value )
+					hdmi_port: bool, \
+				product_owner: User = None,
+					track: bool = True ):
+		if track == True:
+			Screen.__id += 1
+			self._id = Screen.__id
+		else:
+			self._id = -1
+		super().__init__( product_name, product_value, product_owner, track )
 		self._display_size_in: int = display_size
 		self._hdmi_port: bool = hdmi_port
-		self._id = Screen.__id
 
 	def getSize( self ):
 		return ( self._display_size_in )
@@ -172,16 +184,20 @@ class Keyboard( Product ):
 	def __init__(	self, \
 				product_name: str, \
 				product_value: int, \
-				#product_owner: User, \
 			  		wireless: bool, \
 					mechanical: bool, \
-					type: tuple[str,str] ):
-		Keyboard.__id += 1
-		super().__init__( product_name, product_value )
+					type: tuple[str,str],
+				product_owner: User = None, \
+					track: bool = True ):
+		if track == True:
+			Keyboard.__id += 1
+			self._id = Keyboard.__id
+		else:
+			self._id = -1
+		super().__init__( product_name, product_value, product_owner, track )
 		self._wireless: bool = wireless
 		self._mechanical: bool = mechanical
 		self._type = type
-		self._id = Keyboard.__id
 
 class Mouse( Product ):
 	"""
@@ -207,14 +223,18 @@ class Mouse( Product ):
 	def __init__(	self,
 				product_name: str, \
 				product_value: int, \
-				#product_owner: User, \
 					wireless: bool, \
-					button_amount: int ):
-		Mouse.__id += 1
-		super().__init__( product_name, product_value )
+					button_amount: int,
+				product_owner: User = None, \
+					track: bool = True ):
+		if track == True:
+			Mouse.__id += 1
+			self._id = Mouse.__id
+		else:
+			self._id = -1
+		super().__init__( product_name, product_value, product_owner, track )
 		self._wireless: bool = wireless
 		self._button_amount: int = button_amount
-		self._id = Mouse.__id
 
 class Inventory:
 	"""
@@ -258,16 +278,17 @@ class Inventory:
 					return ( product )
 
 	def search_by_monitor( self, size: int, hdmi: bool ):
+		screen_tmp = Screen( "",0,0, False,None,False )
 		for product_type in self._stock:
 			if type( product_type[0] ).__name__ == \
-				type( Screen ).__name__:
+				type( screen_tmp ).__name__:
 				for product in product_type:
 					if product.getSize() == size \
 					and product.getHdmiPort() == hdmi:
 						return ( product )
 
 	def search_by_keyboard_info( self, wireless: bool, mechanical: bool  ):
-		pass
+		keyboard_tmp = Keyboard( "", )
 
 	def search_by_keyboard_type( self, type: str ):
 		pass
