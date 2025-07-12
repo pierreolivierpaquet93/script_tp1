e8dbc4f64b257ee308b65e6b5d28bac4e6163356
Pierre Olivier Paquet <pierreolivierpaquet93@gmail.com>
2025-07-12 09:57:03 -0400

    {04} +Data; +Computer

--- 04_Inventaire_TI/Data.py
+++ 04_Inventaire_TI/Data.py
@@ -1,3 +1,35 @@
 
 # ----------------------------------------------------------------- [ CLASS.ES ]
 
+class User:
+	def __init__( self, name: str ):
+		self._name = name
+
+	def getName( self ):
+		return ( self._name )
+
+class Product:
+	def __init__(	self, \
+			  		name: str, \
+					value: int, \
+					user: User ):
+		self._name:str = name
+		self._value:int = value
+		self._owner = user
+
+class Computer( Product ):
+	def __init__(	self, \
+			  		product_name: str, \
+					product_value: int, \
+					product_user: User, \
+					year: int, \
+					cpu_type: str, \
+					gpu_type: str, \
+					ram_gb: int, \
+					hd_gb: int ):
+		self._year = year
+		self._cpu_type = cpu_type
+		self._gpu_type = gpu_type
+		self._ram_gb = ram_gb
+		self._hd_gb = hd_gb
+		super().__init__( product_name, product_value, product_user )
--- 04_Inventaire_TI/main.py
+++ 04_Inventaire_TI/main.py
@@ -1,5 +1,14 @@
 
+# ----------------------------------------------------------------- [ MODULE.S ]
+
+from Data import	User, \
+					Computer
+
+# --------------------------------------------------------------------- [ MAIN ]
+
 def main():
+	utilisateur = User( "Diego l'Ortho" )
+	ordinateur = Computer( "Nom::Ordinateur", 690, utilisateur, 2024, "Caliss", "Tabarnack", 4, 512 )
 	return ( 0 )
 
 if __name__ == "__main__":
